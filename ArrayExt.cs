57c57eb07a249d375f6c191359bdc73d30ed7ecc
Jake <mllu@tuta.io>
2020-03-09 10:48:58 +0300

    compireto version -1

--- ArrayExt.cs
+++ ArrayExt.cs
@@ -1,13 +1,20 @@
 ﻿namespace System.ArrayExt
-{
+{ 
+    //enum CompireToState: int
+    //{
+    //    LESS = 1,
+    //    ZERO = 0,
+    //    GEATER = -1
+    //} 
+
     public static class ArrayExt
     {
-        public static void SortIComparable<T>(T[] array) where T : IComparable
+        public static void SortIComparable<T>(T[] array, int compireToState = -1 ) where T : IComparable
         {
             T exchange = default;
             for (int i = 0; i < array.Length; i++)
                 for (int j = i + 1; j < array.Length; j++)
-                    if (array[i].CompareTo(array[j]) == -1)
+                    if (array[i].CompareTo(array[j]) == compireToState)
                     {
                         exchange = array[i];
                         array[i] = array[j];
--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ namespace sharpsortarray
         static void Main()
         {
             int[] arrayInt = RandomExt.GetRandomArrayInt(10, -30, 30);
-            ArrayExt.SortIComparable(arrayInt);
+            ArrayExt.SortIComparable(arrayInt,-1);
             ConsoleExt.WriteArrayIFormattable(arrayInt);
 
             Console.WriteLine();
 
             double[] arrayDouble = RandomExt.GetRandomArrayDouble(5);
-            ArrayExt.SortIComparable(arrayDouble);
+            ArrayExt.SortIComparable(arrayDouble,1);
             ConsoleExt.WriteArrayIFormattable(arrayDouble);
 
             Console.ReadKey();
